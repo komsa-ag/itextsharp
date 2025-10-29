ddc516e13a4b0668f3d52b3e18b17abbc171cae2
Lukas Gelke <Lukas.Gelke@komsa.de>
2025-10-29 15:43:56 +0100

    add our metadata, to remember where it originated

---
+++ src/KomsaSharedAssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyMetadata("Komsa: source", "https://github.com/komsa-ag/itextsharp/tree/portable")]
+[assembly: AssemblyMetadata("Komsa: source original author", "https://github.com/danielgindi/Portable.itextsharp5/tree/portable#7772f27")]
